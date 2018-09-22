7da3db86308354d37e956966293ef8ae08dbdd66
Le Thi Quynh Nhi <ltqnhi97@gmail.com>
2018-09-22 22:56:16 +0700

    exercise calculator

--- app/src/main/java/com/lethiquynhnhi/caculator/MainActivity.java
+++ app/src/main/java/com/lethiquynhnhi/caculator/MainActivity.java
@@ -8,178 +8,281 @@ import android.view.inputmethod.BaseInputConnection;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText et;
-    private TextView tv;
+    private EditText edtNumber;
+    private TextView tvResult;
 
-    private Button btn1;
-    private Button btn2;
-    private Button btn3;
-    private Button btn4;
-    private Button btn5;
-    private Button btn6;
-    private Button btn7;
-    private Button btn8;
-    private Button btn9;
-    private Button btn0;
+    private Button btnNumber1;
+    private Button btnNumber2;
+    private Button btnNumber3;
+    private Button btnNumber4;
+    private Button btnNumber5;
+    private Button btnNumber6;
+    private Button btnNumber7;
+    private Button btnNumber8;
+    private Button btnNumber9;
+    private Button btnNumber0;
 
     private Button btnCong;
     private Button btnTru;
     private Button btnNhan;
     private Button btnChia;
-    private Button btnBang;
-    private Button btnCham;
-    private Button btnC;
-    private Button btnAC;
 
-
-
+    private Button btnPoint;
+    private Button btnResult;
+    private Button btnClear;
+    private Button btnClearAll;
+    private final String TAG = getClass().getSimpleName();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initWidget();
-        setEventClickView ();
+        setEventClickViews();
     }
-    public void initWidget()
-    {
-        et = (EditText) findViewById(R.id.et);
-        tv = (TextView) findViewById(R.id.tv);
-        btn0 = (Button) findViewById(R.id.btn0);
-        btn1 = (Button) findViewById(R.id.btn1);
-        btn2 = (Button) findViewById(R.id.btn2);
-        btn3 = (Button) findViewById(R.id.btn3);
-        btn4 = (Button) findViewById(R.id.btn4);
-        btn5 = (Button) findViewById(R.id.btn5);
-        btn6 = (Button) findViewById(R.id.btn6);
-        btn7 = (Button) findViewById(R.id.btn7);
-        btn8 = (Button) findViewById(R.id.btn8);
-        btn9 = (Button) findViewById(R.id.btn9);
+
+    public void initWidget() {
+
+        edtNumber = (EditText) findViewById(R.id.edt_input);
+        tvResult = (TextView) findViewById(R.id.tv_result);
+
+        btnNumber1 = (Button) findViewById(R.id.btnNumber1);
+        btnNumber2 = (Button) findViewById(R.id.btnNumber2);
+        btnNumber3 = (Button) findViewById(R.id.btnNumber3);
+        btnNumber4 = (Button) findViewById(R.id.btnNumber4);
+        btnNumber5 = (Button) findViewById(R.id.btnNumber5);
+        btnNumber6 = (Button) findViewById(R.id.btnNumber6);
+        btnNumber7 = (Button) findViewById(R.id.btnNumber7);
+        btnNumber8 = (Button) findViewById(R.id.btnNumber8);
+        btnNumber9 = (Button) findViewById(R.id.btnNumber9);
+        btnNumber0 = (Button) findViewById(R.id.btnNumber0);
+
         btnCong = (Button) findViewById(R.id.btnCong);
         btnTru = (Button) findViewById(R.id.btnTru);
         btnNhan = (Button) findViewById(R.id.btnNhan);
         btnChia = (Button) findViewById(R.id.btnChia);
-        btnCham = (Button) findViewById(R.id.btnCham);
-        btnC = (Button) findViewById(R.id.btnC);
-        btnAC = (Button) findViewById(R.id.btnAC);
-        btnBang = (Button) findViewById(R.id.btnBang);
+
+        btnPoint = (Button) findViewById(R.id.btnPoint);
+        btnClear = (Button) findViewById(R.id.btnClear);
+        btnClearAll = (Button) findViewById(R.id.btnClearAll);
+        btnResult = (Button) findViewById(R.id.btnResult);
     }
 
-    //dùng để bắt được sự kiện
-    public void setEventClickView ()
-    {
-    btn0.setOnClickListener(this);
-    btn1.setOnClickListener(this);
-    btn2.setOnClickListener(this);
-    btn3.setOnClickListener(this);
-    btn4.setOnClickListener(this);
-    btn5.setOnClickListener(this);
-    btn6.setOnClickListener(this);
-    btn7.setOnClickListener(this);
-    btn8.setOnClickListener(this);
-    btn9.setOnClickListener(this);
-    btnCong.setOnClickListener(this);
-    btnTru.setOnClickListener(this);
-    btnNhan.setOnClickListener(this);
-    btnChia.setOnClickListener(this);
-    btnCham.setOnClickListener(this);
-    btnBang.setOnClickListener(this);
-    btnC.setOnClickListener(this);
-    btnAC.setOnClickListener(this);
+    public void setEventClickViews() {
+        btnNumber1.setOnClickListener(this);
+        btnNumber2.setOnClickListener(this);
+        btnNumber3.setOnClickListener(this);
+        btnNumber4.setOnClickListener(this);
+        btnNumber5.setOnClickListener(this);
+        btnNumber6.setOnClickListener(this);
+        btnNumber7.setOnClickListener(this);
+        btnNumber8.setOnClickListener(this);
+        btnNumber9.setOnClickListener(this);
+        btnNumber0.setOnClickListener(this);
+
+        btnCong.setOnClickListener(this);
+        btnTru.setOnClickListener(this);
+        btnNhan.setOnClickListener(this);
+        btnChia.setOnClickListener(this);
+
+        btnPoint.setOnClickListener(this);
+        btnClear.setOnClickListener(this);
+        btnClearAll.setOnClickListener(this);
+        btnResult.setOnClickListener(this);
+
+
     }
 
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
-            case R.id.btn0:
+            case R.id.btnNumber0:
                 //TO DO
-                et.append("0");
+                edtNumber.append("0");
                 break;
-            case R.id.btn1:
+            case R.id.btnNumber1:
                 //TO DO
-                et.append("1");
+                edtNumber.append("1");
                 break;
-            case R.id.btn2:
+            case R.id.btnNumber2:
                 //TO DO
-                et.append("2");
+                edtNumber.append("2");
                 break;
-            case R.id.btn3:
+            case R.id.btnNumber3:
+                edtNumber.append("3");
                 //TO DO
-                et.append("3");
                 break;
-            case R.id.btn4:
+            case R.id.btnNumber4:
                 //TO DO
-                et.append("4");
+                edtNumber.append("4");
                 break;
-            case R.id.btn5:
+            case R.id.btnNumber5:
                 //TO DO
-                et.append("5");
+                edtNumber.append("5");
                 break;
-            case R.id.btn6:
+            case R.id.btnNumber6:
                 //TO DO
-                et.append("6");
+                edtNumber.append("6");
                 break;
-            case R.id.btn7:
+            case R.id.btnNumber7:
                 //TO DO
-                et.append("7");
+                edtNumber.append("7");
                 break;
-            case R.id.btn8:
+            case R.id.btnNumber8:
                 //TO DO
-                et.append("8");
+                edtNumber.append("8");
                 break;
-            case R.id.btn9:
+            case R.id.btnNumber9:
                 //TO DO
-                et.append("9");
+                edtNumber.append("9");
                 break;
             case R.id.btnCong:
                 //TO DO
-                et.append("+");
+                edtNumber.append("+");
                 break;
             case R.id.btnTru:
                 //TO DO
-                et.append("-");
+                edtNumber.append("-");
                 break;
             case R.id.btnNhan:
                 //TO DO
-                et.append("*");
+                edtNumber.append("*");
                 break;
             case R.id.btnChia:
                 //TO DO
-                et.append("/");
+                edtNumber.append("/");
                 break;
-            case R.id.btnCham:
+
+            case R.id.btnPoint:
                 //TO DO
-                et.append(".");
+                edtNumber.append(".");
                 break;
-            case R.id.btnC:
+            case R.id.btnClear:
                 //TO DO
-              /*C1:  Xóa kí tự cuối cùng khi click nút C
-               String numberAfterRemove = deleteANumber(et.getText().toString());
-               et.setText(numberAfterRemove);*/
-                BaseInputConnection textFileInputConnection = new BaseInputConnection(et, true);
-                textFileInputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL));
+                /*C1:  Xóa kí tự cuối cùng khi click nút C
+                    String numberAfterRemove = deleteANumber(et.getText().toString());
+                     et.setText(numberAfterRemove);*/
+                BaseInputConnection textFieldInputConnection = new BaseInputConnection(edtNumber, true);
+                textFieldInputConnection.sendKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_DEL));
                 break;
-            case R.id.btnAC:
+            case R.id.btnClearAll:
                 //TO DO
-                et.setText("");
+                edtNumber.setText("");
+                tvResult.setText("");
+
                 break;
-            case R.id.btnBang:
-                //TO DO
+            case R.id.btnResult:
+                //To do
                 // Lấy số vừa mới nhập chuyển sang kiểu string để sau có thể chuyển số vừa nhập sang kiểu int
                 //String numberA = et.getText().toString();
-                int numberA = Integer.parseInt(et.getText().toString());
-                break;
+                //int numberA = Integer.parseInt(et.getText().toString());
+
+                DecimalFormat df = new DecimalFormat("###.#######");
+                double result = 0;
+                addOperation(edtNumber.getText().toString());
+                addNumber(edtNumber.getText().toString());
+                // Thuật toán tính toán biểu thức
+                if(arrOperation.size()>=arrNumber.size() ||arrOperation.size()<1){
+                    tvResult.setText("Lỗi định dạng");
+                }else {
+                    for (int i = 0; i < (arrNumber.size() - 1); i++) {
+                        switch (arrOperation.get(i)) {
+                            case "+":
+                                if (i == 0) {
+                                    result = arrNumber.get(i) + arrNumber.get(i + 1);
+                                } else {
+                                    result = result + arrNumber.get(i + 1);
+                                }
+                                break;
+                            case "-":
+                                if (i == 0) {
+                                    result = arrNumber.get(i) - arrNumber.get(i + 1);
+                                } else {
+                                    result = result - arrNumber.get(i + 1);
+                                }
+                                break;
+                            case "*":
+                                if (i == 0) {
+                                    result = arrNumber.get(i) * arrNumber.get(i + 1);
+                                } else {
+                                    result = result * arrNumber.get(i + 1);
+                                }
+                                break;
+                            case "/":
+                                if (i == 0) {
+                                    result = arrNumber.get(i) / arrNumber.get(i + 1);
+                                } else {
+                                    result = result / arrNumber.get(i + 1);
+                                }
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+                    tvResult.setText(df.format(result) + "");
+                }
+
+                // tvResult.setText(df.format(result) + "");
+                //  edtNumber.setText("");
+                //  result = 0;
+//        }
+        }
+
+    }
+    /*Cách 1:
+Xóa kí tự cuối cùng khi click nút C
+public String deleteANumber(String number)
+{
+    int length = number.length();
+    String ten = number.substring(0,length - 1);
+    return ten;
+}*/
+
+    //Mảng chứa các phép tính +, - , x, /
+    public ArrayList<String> arrOperation;
+    //Mảng chứa các số
+    public ArrayList<Double> arrNumber;
+
+    //Lấy tất cả các phép tính lưu vào mảng arrOperation
+    public int addOperation(String input) {
+        arrOperation = new ArrayList<>();
+        //tạo ra 1 mảng chứa cấc kí tự
+        char[] cArray = input.toCharArray();
+        for (int i = 0; i < cArray.length; i++) {
+            switch (cArray[i]) {
+                case '+':
+                    arrOperation.add(cArray[i] + "");
+                    break;
+                case '-':
+                    arrOperation.add(cArray[i] + "");
+                    break;
+                case '*':
+                    arrOperation.add(cArray[i] + "");
+                    break;
+                case '/':
+                    arrOperation.add(cArray[i] + "");
+                    break;
+                default:
+                    break;
+            }
+        }
+        return 0;
+    }
+    //Lấy tất cả các số lưu vào mảng arrNumber
+    public void addNumber(String stringInput) {
+        arrNumber = new ArrayList<>();
+        //lấy các số ra theo định dạng (\d+(?:\.\d+)?)
+        Pattern regex = Pattern.compile("(\\d+(?:\\.\\d+)?)");
+        Matcher matcher = regex.matcher(stringInput);
+        while(matcher.find()){
+            arrNumber.add(Double.valueOf(matcher.group(1)));
         }
     }
 
-    /*Cách 1:
-    Xóa kí tự cuối cùng khi click nút C
-    public String deleteANumber(String number)
-    {
-        int length = number.length();
-        String ten = number.substring(0,length - 1);
-        return ten;
-    }*/
 }
